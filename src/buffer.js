ccf79808df963aff04c6678fc2f57dba55092f30
holo <holo@HOLO-PC>
2019-06-16 11:56:55 +0400

    Added initial files

---
+++ src/buffer.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("# Emit the buffer 1000ms");
+// interval.apply(100)
+//     .pipe(
+//         buffer(interval(1000)),
+//         take(3)
+//     ).subscribe(console.log)
---
+++ src/buffer.ts
@@ -0,0 +1,8 @@
+
+
+console.log("# Emit the buffer 1000ms")
+// interval.apply(100)
+//     .pipe(
+//         buffer(interval(1000)),
+//         take(3)
+//     ).subscribe(console.log)
